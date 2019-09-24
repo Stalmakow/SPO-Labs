845fd0670da181326b29d2769038cf9919e310d3
Stalmakow <55708966+Stalmakow@users.noreply.github.com>
2019-09-24 10:51:07 +0300

    Update nik2.sh

--- nik2.sh
+++ nik2.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
-(echo "Name,Format,Size,Creat-time,Change-time,Type,Audio-Video Duration")>./Data.xls
-rm -r ~/Downloads
-files()
+(echo "Name,Format,Size,Creat-time,Change-time,Type,Audio-Video Duration")>./Data.xls   #это комментарий к коду
+rm -r ~/Downloads   
+files()   # ну в общем крч это функция, и она функционирует
 {
-(
-for i in "$1"/*
+( 
+for i in "$1"/*       
 do
 N1=$(expr length "$i")
 N2=3
@@ -16,9 +16,9 @@ w=$N3
 fi
 s=$i
 e=$(($N1+$N2))
-if [[ $w == "mkv" || $w == "mp4" ]]; then
+if [[ $w == "mkv" || $w == "mp4" ]]; then         #Самая большая и болезненная часть скрипта- определение на видео-аудио
 echo $(stat --printf="%n" "$s"|rev|cut -f1 -d /|rev),$w,$(stat --printf="%s,%w,%z" "$s"),$(file "$s" | cut -c$e-| sed 's/,//g'
-),$(mediainfo "$s"|head -n7|tail -n1|cut -c44-)
+),$(mediainfo "$s"|head -n7|tail -n1|cut -c44-)   #Сразу скажу идея этого определения не моя, я ее поташил у друга потому что нашел приемлемой
 elif [[ $w == "mp3" ]]; then
 echo $(stat --printf="%n" "$s"|rev|cut -f1 -d /|rev),$w,$(stat --printf="%s,%w,%z" "$s"),$(file "$s" | cut -c$e-| sed 's/,//g'
 ),$(mediainfo "$s"|head -n5|tail -n1|cut -c44-)
