4c2b07996079f345f94ccb3a60866bdc00f56281
Stalmakow <55708966+Stalmakow@users.noreply.github.com>
2019-09-30 19:23:03 +0300

    Update skript.sh

--- vtorayalaba/skript.sh
+++ vtorayalaba/skript.sh
@@ -1,8 +1,8 @@
-#!/bin/bash
-mkdir /stalmakowproject
-cd /stalmakowproject
+#!/bin/bash     #ну это крч скрипт в том чтобы подкачать требуемых для ихнего скрипта прог и запустить ихний скрипт
+mkdir /stalmakowproject     #создаю место под то что скачаю с гита и соответсвенно скачиваю и "развертываю" ето
+cd /stalmakowproject        
 git init
 git remote add origin https://github.com/popcorn-official/popcorn-desktop
 git pull https://github.com/popcorn-official/popcorn-desktop
-apt install npm
-./makepopcorn.sh
+apt install npm   #подкачиваю элемент npm
+./makepopcorn.sh  #подрубаю их скрипт 
